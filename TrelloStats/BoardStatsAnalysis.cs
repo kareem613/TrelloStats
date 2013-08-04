41682ebb00754d7b784b6bc8dc8dd57ba84fb689
Kareem Sultan <kareemsultan@gmail.com>
2013-08-04 16:32:52 -0400

    moved weekstats class into it's own file

--- TrelloStats/BoardStatsAnalysis.cs
+++ TrelloStats/BoardStatsAnalysis.cs
@@ -115,52 +115,5 @@ namespace TrelloStats
 
         internal BoardProjections Projections { get; set; }
     }
-  
-    public class WeekStats
-    {
-        public IEnumerable<CardStats> Cards { get; set; }
-        public IEnumerable<CardStats> CardsInProgress { get; set; }
-        public IEnumerable<CardStats> CardsInTest{ get; set; }
-
-        public int WeekNumber { get; set; }
-        public int NumberOfCompletedCards
-        {
-            get
-            {
-                return Cards.Count();
-            }
-        }
-
-        public double PointsCompleted
-        {
-            get
-            {
-                return Cards.Sum(c => c.CardData.Points);
-            }
-        }
-
-        public int GetNumberOfCardsWithLabel(string label)
-        {
-            return Cards.Count(c => c.CardData.Card.Labels.Any(l => l.Name == label));
-        }
-
-        public int NumberOfCardsInProgress
-        {
-            get
-            {
-                return CardsInProgress.Count();
-            }
-        }
-
-        public int NumberOfCardsInTest
-        {
-            get
-            {
-                return CardsInTest.Count();
-            }
-        }
-        public DateTime StartDate { get; set; }
-
-        public DateTime EndDate { get; set; }
-    }
+    
 }
---
+++ TrelloStats/Model/Stats/WeekStats.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrelloStats.Model.Stats
+{
+    public class WeekStats
+    {
+        public IEnumerable<CardStats> Cards { get; set; }
+        public IEnumerable<CardStats> CardsInProgress { get; set; }
+        public IEnumerable<CardStats> CardsInTest { get; set; }
+
+        public int WeekNumber { get; set; }
+        public int NumberOfCompletedCards
+        {
+            get
+            {
+                return Cards.Count();
+            }
+        }
+
+        public double PointsCompleted
+        {
+            get
+            {
+                return Cards.Sum(c => c.CardData.Points);
+            }
+        }
+
+        public int GetNumberOfCardsWithLabel(string label)
+        {
+            return Cards.Count(c => c.CardData.Card.Labels.Any(l => l.Name == label));
+        }
+
+        public int NumberOfCardsInProgress
+        {
+            get
+            {
+                return CardsInProgress.Count();
+            }
+        }
+
+        public int NumberOfCardsInTest
+        {
+            get
+            {
+                return CardsInTest.Count();
+            }
+        }
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
